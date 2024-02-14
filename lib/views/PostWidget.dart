270385c5df9a3e09290cbbafc51fec68e11e0737
Jenny Kim <yjkim@usc.edu>
2024-02-13 18:52:59 -0800

    change format of post widget

--- lib/views/PostWidget.dart
+++ lib/views/PostWidget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart'; // Import the intl package
 import 'package:pueblo_del_rio/models/post.dart';
 
 class PostWidget extends StatelessWidget {
@@ -8,19 +9,61 @@ class PostWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    String bodyPreview = post.body.length > 150 ? '${post.body.substring(0, 150)}...' : post.body;
+    String bodyPreview = post.body.length > 250 ? '${post.body.substring(0, 250)}...' : post.body;
+
+    // Format the date and time
+    String formattedDate = post.date != null ? DateFormat('MMMM dd, yyyy hh:mm a').format(post.date!) : 'N/A';
 
     return Container(
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.grey),
-        borderRadius: BorderRadius.circular(8.0),
-      ),
-      padding: const EdgeInsets.all(8.0),
       margin: const EdgeInsets.symmetric(vertical: 8.0),
-      child: ListTile(
-        title: Text(post.title),
-        subtitle: Text(bodyPreview),
-        trailing: const Icon(Icons.keyboard_arrow_right),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey), // border
+      ),
+      child: SizedBox(
+        width: double.infinity, // full width
+        child: ListTile(
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '$formattedDate', // Use the formatted date
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                    ),
+                  ),
+                  Text(
+                    'Author: ${post.author}',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 8),
+              Text(
+                post.title,
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.blue,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 8),
+              Text(
+                bodyPreview,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
